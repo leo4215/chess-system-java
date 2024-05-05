5b48d0ae635e971460bbbe375e937bfcf12719dc
Leonardo de Camargo <leonardo4215@live.com>
2024-05-05 10:45:09 -0400

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,20 @@
+package boardgame;
+
+public abstract class Piece { 
+    
+    protected Position position; //Only visible in board layer 
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board; //Informing a board when creating a piece
+        position = null; 
+        //The position of a newly created piece will initially be null meaning that it's not placed on the board yet
+    }
+    
+    protected Board getBoard() {
+    //The board must be used only within the board layer, and not the chess layer
+        return board;
+    }
+
+
+}
