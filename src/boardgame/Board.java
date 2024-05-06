0120adaab2d5d5d32a0601ad8a444d7bd1928359
Leonardo de Camargo <leonardo4215@live.com>
2024-05-06 15:18:26 -0400

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,10 +6,10 @@ public class Board {
     private int columns;
     private Piece[][] pieces;
     
-    public Board(int rows, int columns) {
+    public Board(int rows, int columns) { //Rows and columns responsible for setting the board size
         this.rows = rows;
         this.columns = columns;
-        pieces = new Piece[rows][columns];
+        pieces = new Piece[rows][columns]; //Instantiates the board pieces matrix given the entered rows and columns number
     }
 
     public int getRows() {
@@ -28,11 +28,16 @@ public class Board {
         this.columns = columns;
     }
 
-    public Piece piece(int row, int column) {
+    public Piece piece(int row, int column) { //Returns a piece by receiving the row and column numbers as a parameter
         return pieces[row][column];
     }
 
-    public Piece piece(Position position) {
+    public Piece piece(Position position) { //Overloading: returns a piece by receiving a type Position as a parameter
         return pieces[position.getRow()][position.getColumn()];
+    }
+
+    public void placePiece(Piece piece, Position position) { //Assigning the piece to the given matrix position
+        pieces [position.getRow()][position.getColumn()] = piece; 
+        piece.position = position;
     }
 }
